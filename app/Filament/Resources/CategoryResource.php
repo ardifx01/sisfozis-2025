ba10606effa8afbb12d5c04f17ba457587d5a70d
Anggra Aditra <aditra.anggra@gmail.com>
2024-10-29 17:07:44 +0700

    project init

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    protected static ?string $navigationGroup = 'Administrasi Surat';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                //card
+                Forms\Components\Card::make()
+                    ->schema([
+
+                        //name
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nama Kategori')
+                            ->placeholder('Nama Kategori..')
+                            ->required(),
+
+                        //description
+                        Forms\Components\Textarea::make('desc')
+                            ->label('Keterangan')
+                            ->placeholder('Keterangan')
+                            ->rows(5),
+
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('desc'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
