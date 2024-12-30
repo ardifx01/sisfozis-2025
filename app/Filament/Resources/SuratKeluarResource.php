0a8610e199488c4c70284fa13f374ddbb31f8a86
Anggra Aditra <aditra.anggra@gmail.com>
2024-12-30 19:46:01 +0700

    remove view on resource table

--- app/Filament/Resources/SuratKeluarResource.php
+++ app/Filament/Resources/SuratKeluarResource.php
@@ -90,7 +90,6 @@ class SuratKeluarResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
--- app/Filament/Resources/SuratMasukResource.php
+++ app/Filament/Resources/SuratMasukResource.php
@@ -151,7 +151,6 @@ class SuratMasukResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
